851ac5c1e121e0d8d26e90584819d3b00666a9b5
robwatsongtr <rwatso@gmail.com>
2022-02-04 20:47:58 -0800

    getting in async await

---
+++ promises.js
@@ -0,0 +1,72 @@
+const posts = [
+  { 
+    title : 'Post one',
+    body : 'this is post one content'
+  },
+  { 
+    title : 'Post two',
+    body : 'this is post two content'
+  }   
+];
+
+
+function getPosts() {
+  setTimeout( () => {
+    let output = '';
+
+    // append each post hence the +=
+    posts.forEach( (post, index) => {
+      output += `<li>${post.title}</li>`
+    });
+
+    document.body.innerHTML = output; // insert into DOM 
+  }, 1000);
+}
+
+
+function createPost(post) {
+  return new Promise( (resolve, reject) => {
+    setTimeout( () => {
+
+      posts.push(post);
+
+      const error = false; 
+
+      if(!error) {
+        resolve()
+      } else {
+        reject('Error: Something went wrong')
+      }
+
+    }, 2000);
+  });
+}
+
+
+// createPost({ title: 'Post three', body: 'this is post three yo' })
+//   .then(getPosts)
+//   .catch( err =>  console.log(err) )
+
+
+// Async / Await
+async function init() {
+  await createPost({ title: 'Post three', body: 'this is post three yo' });
+  getPosts()
+}
+
+init(); 
+
+
+// Promise.all
+// const promise1 = Promise.resolve('Hello World');
+// const promise2 = 10; 
+// const promise3 = new Promise( (resolve, reject) => 
+// setTimeout(resolve, 2000, 'Goodbye') )
+
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+//   .then( res => res.json() )
+
+// Promise.all takes in an array of promises 
+
+// Promise.all([promise1, promise2, promise3, promise4])
+//   .then(values => console.log(values));
